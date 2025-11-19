20fd44d5241405258ac9de6db6a4c0355c114202
zagadou <86722630+zagadou92@users.noreply.github.com>
2025-11-19 07:06:53 -1100

    Delete routes directory

--- routes/swagger.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.json');
-
-router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-module.exports = router;
